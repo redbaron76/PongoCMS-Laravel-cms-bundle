945ee6cb9805590781e661500c47c62061fd494a
redbaron76 <f.fumis@gmail.com>
2012-08-20 12:15:46 +0200

    Banner marker fixed

--- views/theme/default/partials/markers/banner.blade.php
+++ views/theme/default/partials/markers/banner.blade.php
@@ -3,12 +3,22 @@
 	@foreach($images as $key => $image)
 	<li>
 		
-		<?php $target = ((bool) $image->pivot->is_blank) ? ' target="_blank"' : '' ?>
-		<?php $title = (count($attr[$key]->filetexts[0]->title) > 0) ? ' title="'.$attr[$key]->filetexts[0]->title.'"' : '' ?>
+		<?php
+
+			$target = ((bool) $image->pivot->is_blank) ? ' target="_blank"' : '';
+			$title = '';
+			$alt = '';
+
+			if(!empty($attr[$key]->filetexts)) {
+				$title = (strlen($attr[$key]->filetexts[0]->title) > 0) ? ' title="'.$attr[$key]->filetexts[0]->title.'"' : '';
+				$alt = (strlen($attr[$key]->filetexts[0]->alt) > 0) ? $attr[$key]->filetexts[0]->alt : '';
+			}
+
+		?>
 
 		<a href="{{SLUG($image->pivot->url)}}"{{$target}}{{$title}}>
 
-			{{HTML::image($image->path, $attr[$key]->filetexts[0]->alt, array('width' => $image->w, 'height' => $image->h))}}
+			{{HTML::image($image->path, $alt, array('width' => $image->w, 'height' => $image->h))}}
 			
 		</a>
 	</li>
--- views/theme/default/partials/markers/nivo.blade.php
+++ views/theme/default/partials/markers/nivo.blade.php
@@ -8,21 +8,43 @@
 
 			<?php
 
-				$titles[$key] = ($caption and strlen($attr[$key]->filetexts[0]->caption) > 0) ? '#'.Str::random(10, 'alpha') : '';
+				$target = ((bool) $image->pivot->is_blank) ? ' target="_blank"' : '';
+				$title = '';
+				$alt = '';
+
+				if(!empty($attr[$key]->filetexts)) {
+
+					$titles[$key] = ($caption and strlen($attr[$key]->filetexts[0]->caption) > 0) ? '#'.Str::random(10, 'alpha') : '';
+					$title = (strlen($attr[$key]->filetexts[0]->title) > 0) ? ' title="'.$attr[$key]->filetexts[0]->title.'"' : '';
+					$alt = (strlen($attr[$key]->filetexts[0]->alt) > 0) ? $attr[$key]->filetexts[0]->alt : '';
+
+				} else {
+
+					$titles[$key] = '';
+
+				}
 
 			?>
 
-			{{HTML::image($image->path, $attr[$key]->filetexts[0]->alt, array('width' => $image->w, 'height' => $image->h, 'title' => $titles[$key]))}}
+			@if(strlen($image->pivot->url) > 0)
+			<a href="{{SLUG($image->pivot->url)}}"{{$target}}{{$title}}>
+			@endif
+
+				{{HTML::image($image->path, $alt, array('width' => $image->w, 'height' => $image->h, 'title' => $titles[$key]))}}
+
+			@if(strlen($image->pivot->url) > 0)
+			</a>
+			@endif
 
 		@endforeach
 
 	</div>
 
-	@if($caption)
+	@if($caption and !empty($titles))
 
 		@foreach($images as $key => $image)
 
-			@if(strlen($attr[$key]->filetexts[0]->caption) > 0)
+			@if(!empty($attr[$key]->filetexts) and strlen($attr[$key]->filetexts[0]->caption) > 0)
 
 				<div id="{{str_replace('#','',$titles[$key])}}" class="nivo-html-caption">
 					{{$attr[$key]->filetexts[0]->caption}}
