37e6604d4be161dd711cd74abbaa164384b760e8
redbaron76 <f.fumis@gmail.com>
2012-10-02 15:54:33 +0200

    PongoCMS v1.0.9

---
+++ views/theme/default/sample/page.php
@@ -0,0 +1,9 @@
+<h1>Titile</h1>
+
+<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper. Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+<h3>Subtitle</h3>
+
+<p>Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam quis risus eget urna mollis ornare vel eu leo. Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida at eget metus. Sed posuere consectetur est at lobortis.</p>
+
+<p>Nulla vitae elit libero, a pharetra augue. Donec sed odio dui. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Curabitur blandit tempus porttitor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
