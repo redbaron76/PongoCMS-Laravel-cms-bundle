14fb812ed6918dc183f5930241348468fd818c0b
redbaron76 <f.fumis@gmail.com>
2012-10-12 18:00:11 +0200

    PongoCMS release 1.1.3

--- libraries/utilities.php
+++ libraries/utilities.php
@@ -284,7 +284,6 @@ class CmsUtility {
 	*/
 	public static function link_active($slug)
 	{
-		if (SLUG_FULL == '/' and $slug == SITE_HOMEPAGE) return HTML::attributes(array('class' => 'active'));
 		if (SLUG_FULL == $slug) return HTML::attributes(array('class' => 'active'));
 	}
 
@@ -295,6 +294,7 @@ class CmsUtility {
 	*/
 	public static function link_menu_active($slug)
 	{
+		if (SLUG_FULL == '/' and $slug == SITE_HOMEPAGE) return HTML::attributes(array('class' => 'active'));
 		if (substr_count(SLUG_FULL.'/', $slug.'/') > 0) return HTML::attributes(array('class' => 'active'));
 	}
 
