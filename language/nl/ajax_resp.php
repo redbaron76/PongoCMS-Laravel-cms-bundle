dfe5a857194e756dfe3de97b0d0a46bae2b834b9
Beau Elstgeest <beau.elstgeest@gmail.com>
2019-10-18 15:56:31 +0200

    add nl translation

---
+++ language/nl/ajax_resp.php
@@ -0,0 +1,91 @@
+<?php
+
+return array(
+
+	"ownership_error" => "U bent <strong>niet bevoegd</strong> om dit bestand te bewerken",
+
+	"page_settings_success" => "Instellingen opgeslagen!",
+	"page_settings_error" => "Instellingen error!",
+
+	"page_design_success" => "Design opgeslagen!",
+	"page_design_error" => "Design error!",
+
+	"page_seo_success" => "SEO opgeslagen!",
+	"page_seo_error" => "SEO error!",
+
+	"page_preview_success" => "Voorproef opgeslagen!",
+	"page_preview_error" => "Voorproef error!",
+
+	"page_upload_success" => "Geladen",
+	"page_upload_error" => "Laden error!",
+	"page_upload_ftp" => "Upload dit bestand met FTP in :pad",
+
+	"element_success" => "Element opgeslagen!",
+	"element_error" => "Element error!",
+
+	"role_save_success" => "Rol opgeslagen!",
+	"role_save_error" => "Rol error!",
+
+	"user_account_success" => "Gebruiker opgeslagen!",
+	"user_account_error" => "Gebruiker error!",
+
+	"user_password_success" => "Wachtwoord opgeslagen!",
+	"user_password_error" => "Wachtwoord error!",
+	"user_nouser_error" => "Gebruiker nog niet aangemaakt!",
+
+	"user_details_success" => "Gebruiker details opgeslagen!",
+	"user_details_error" => "Gebruiker details niet opgeslagen!",
+
+	"filetext_text_success" => "Beschrijvingen opgeslagen!",
+	"filetext_text_error" => "Beschrijvingen error!",
+
+	"filename_filename_success" => "Bestand hernoemd!",
+	"filename_filename_error" => "Bestand niet hernoemd!",
+
+	"filename_fileavailable_success" => "File gelinkt!",
+	"filename_fileavailable_error" => "File niet gelinkt!",
+
+	"translation_save_success" => "Vertaling vastgesteld!",
+	"translation_save_error" => "Vertaling niet vastgesteld!",
+	"translation_delete_success" => "Vertaling verwijderd!",
+	"translation_delete_error" => "Vertaling niet verwijderd!",
+
+	"menu_save_success" => "Menu opgeslagen!",
+	"menu_save_error" => "Menu niet opgeslagen!",
+
+	"page_relation_success" => "Pagina's gelinkt!",
+	"page_relation_error" => "Pages niet gelinkt!",
+
+	"banner_save_success" => "Banner geupdate!",
+	"banner_save_error" => "Banner niet geupdate!",
+
+	"download_save_success" => "Download lijst geupdate!",
+	"download_save_error" => "Download lijst niet geupdate!",
+
+	"gallery_save_success" => "Gallerij bijgewerkt!",
+	"gallery_save_error" => "Gallerij niet bijgewerkt!",
+
+	"blog_post_success" => "Blog opgeslagen!",
+	"blog_post_error" => "Blog niet opgeslagen!",
+
+	"blog_preview_success" => "Voorvertoning opgeslagen!",
+	"blog_preview_error" => "Voorvertoning niet opgeslagen!",
+
+	"blog_available_success" => "Post nu beschikbaar!",
+	"blog_available_error" => "Post niet beschikbaar!",
+
+	"blog_relation_success" => "Post gelinkt!",
+	"blog_relation_error" => "Post niet gelinkt!",
+
+	"blog_tags_success" => "Tag gelinkt!",
+	"blog_tags_error" => "Tag niet gelinkt!",
+
+	"tag_save_success" => "Tag aangemaakt!",
+	"tag_save_error" => "Tag niet aangemaakt!",
+
+	// CALENDAR
+
+	"calendar_success" => "Cursus oopgeslagen!",
+	"calendar_error" => "Cursus niet opgeslagen!",
+
+);
