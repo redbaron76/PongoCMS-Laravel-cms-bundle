b85844d12eb6d4d5511aa6f29c524cea0b52e8a9
Beau Elstgeest <beau.elstgeest@gmail.com>
2019-10-18 16:09:27 +0200

    add nl translation for title

---
+++ language/nl/title.php
@@ -0,0 +1,126 @@
+<?php
+
+return array(
+	
+	"preview_title" => "VOORPROEF => ",
+
+	"loginnow" => "Alstublieft, login nu",
+	"homepage" => "Home Pagina",
+	"slogan" => "Inhoud manager",
+	"logout" => "Logout",
+
+	"dashboard"   => "Overzicht",
+	"analytics" => "Dagelijkse bezoeken in de laatste 30 dagen <small>[ van Google Analytics ]</small>",
+	"sitestats" => "Gebruiks stats",
+	"siteflags" => "Algemene instellingen",
+	"blogs" => "Blog management",
+	"banners"   => "Banner management",
+	"downloads"   => "Download management",
+	"files"   => "Media management",
+	"galleries"   => "Gallerij management",
+	"pages" => "Pagina management",
+	"pageitems" => "Element pagina management",
+	"users" => "Gebruiker management",
+	"roles" => "Rol management",
+	"menus" => "Menu management",
+	"tags" => "Tag management",
+	"translations" => "Vertaling management",
+	"orders" => "Volgorde management",
+	"products" => "Product management",
+	"shopsettings" => "Winkel instellingen",
+	
+	// USERS
+	
+	"users_new" => "Nieuwe gebruiker",
+	"users_edit" => "Wijzig gebruiker",
+	"filterbyrole" => "Filter op rol",
+	"otherversion" => "Meer versies",
+	
+	// ROLES
+	
+	"role_new" => "Nieuwe rol",
+	"role_edit" => "Wijzig rol",
+	
+	// ORDERS
+	
+	"order_details" => "Volgorde details",
+	"filterbystatus" => "Filter op status",
+	"filterbyvalue" => "Filter op waarde",
+	
+	// PAGES
+	
+	"page_new" => "Maak een nieuwe pagina",
+	"page_edit" => "wijzig pagina",
+	"item_new" => "Nieuw element",
+	"item_edit" => "Wijzig element",
+	"item_create" => "Maak een nieuw element",
+	"filterbyaccess" => "Filter op toegang",
+	"filterbyflag" => "Filter op vlag",
+
+	"popover_title_page" => "Pagina details",
+
+	// ELEMENTS
+
+	"element_new" => "Maak een nieuw element",
+	"element_edit" => "Wijzig element",
+	
+	// NEWS
+	
+	"blog_new" => "Nieuwe post",
+	"blog_edit" => "Wijzig post",
+	
+	// PRODUCTS
+	
+	"product_new" => "Nieuw product",
+	"product_edit" => "Wijzig product",
+	"filterbycat" => "Filter op categorie",
+	"filterbyprice" => "Filter op prijs",
+	
+	// FILES
+
+	"chooseimage" => "Kies afbeelding",
+	"choosefile" => "Kies bestand",
+	"media_new" => "Nieuwe media",
+	"file_edit" => "Wijzig media",
+	"media_create" => "Pas afbeelding aan",
+	"filterbytype" => "Filter op soort",
+
+	"popover_title_media" => "Bestand details",
+
+	//TRANSLATION
+
+	"current_translation" => "Vertaalde items",
+
+	//MENU
+
+	"menu_new" => "Nieuw menu",
+	"menu_edit" => "Wijzig menu",
+
+	//BANNER
+
+	"banner_new" => "Nieuwe banner",
+	"banner_edit" => "Wijzig banner",
+	"banner_files_active" => "Actieve banners",
+
+	//DOWNLOAD
+
+	"download_new" => "Nieuwe download",
+	"download_edit" => "Wijzig download",
+	"download_files" => "Beschikbare bestanden",
+
+	//GALLERY
+
+	"gallery_new" => "Nieuwe gallerij",
+	"gallery_edit" => "Wijzig gallerij",
+	"gallery_files" => "Afbeeldingen in gallerij",
+
+	//BLOG
+
+	"popover_title_blog" => "Post details",
+
+	// TAGS
+	
+	"tag_new" => "Nieuwe tag",
+	"tag_edit" => "Wijzig tag",
+	
+);
