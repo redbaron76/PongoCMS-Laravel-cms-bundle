489bb63bb552711d07d5760f0c12bdf3ae80f6ee
Beau Elstgeest <beau.elstgeest@gmail.com>
2019-10-18 16:04:03 +0200

    add nl translation for marker

---
+++ language/nl/marker.php
@@ -0,0 +1,40 @@
+<?php
+
+return array(
+
+	"back_descr" => "Maak een \"Ga terug\"-link aan.",
+	"banner_descr" => "Maak een bannerrotator aan zoals opgeslagen in \"Diensten => Banner\\" .".",
+	"blogview_descr" => "Maak een lijst van de laatste blogberichten die op cronologische wijze zijn geordend.",
+	"crumb_descr" => "Een paneermeelnavigatie creëren.",
+	"disqus_descr" => "Schakel het DISQUS-commentaarsysteem voor deze functie in.",
+	"downlist_descr" => "Maak een downloadlijst aan zoals opgeslagen in \"Diensten => Download\"..",
+	"element_descr" => "Dubbele elementinhoud op basis van de element id.",
+	"gallery_descr" => "Maak een fotogalerij zoals opgeslagen in \"Diensten => Galerij\".",
+	"map_descr" => "Maak een Google Map aan.",
+	"menu_descr" => "Maak een menu aan zoals opgeslagen in \"Diensten => Menu\".",
+	"menu_custom_descr" => "Maak een aangepast menu.",
+	"menu_sub_descr" => "Creëer een statisch menu met een link binnen de pagina-elementen.",
+	"preview_descr" => "Previews van berichten, pagina's, etc. krijgen. En maak ze doorzoekbaar door middel van paginering.",
+	"prevnext_descr" => "Maak een paginator \"prev / next\" om door pagina's te bladeren.",
+	"social_descr" => "Maak sociale knoppen aan.",
+	"translation_descr" => "Maak een vertaald uittreksel zoals opgeslagen in \"Diensten => Vertaling\".",
+	"tweets_descr" => "Maak een lijst met de laatste tweets voor die gebruikersnaam.",
+	"video_descr" => "Maak een YouTube-, Vimeo- of Screenr-ingebedde video of een videospeler voor lokale videobestanden.",
+
+	//CRUMB
+
+	"crumb_here" => "hier",
+
+	//SEARCH
+	
+	"search" => "Zoeken",
+	"search_placeholder" => "Zoek iets...",
+	"search_results_title" => "Zoek resultaten",
+	"search_found" => "Zoek resultaten voor: ':q'",
+	"search_not_found" => "Geen resultaten gevonden!",
+
+	//LOGIN
+
+	"login_error" => "Gebruiker: gebruiker: gebruiker niet toegestaan!",
+
+);
